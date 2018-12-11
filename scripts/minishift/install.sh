64025e9047889bb496ae4d16ce628f9d2bddbe0f
George Gastaldi <gegastaldi@gmail.com>
2018-12-11 15:51:08 -0200

    chore: add installation scripts

---
+++ scripts/minishift/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo This script will install Fabric8 Launcher in Minishift. Make sure that:
+echo 
+echo  - Minishift is running 
+echo  - You have run oc login previously
+echo  - Your GitHub Username is correct: $(git config github.user)
+echo  - Your GitHub Token  is correct: $(git config github.token)
+echo 
+echo Press any key to continue ...
+read 
+
+echo Creating launcher project ...
+oc new-project launcher
+
+echo Processing the template and installing ...
+oc process --local -f openshift/launcher-template.yaml \
+   LAUNCHER_KEYCLOAK_URL= \
+   LAUNCHER_MISSIONCONTROL_GITHUB_USERNAME=$(git config github.user) \
+   LAUNCHER_MISSIONCONTROL_GITHUB_TOKEN=$(git config github.token) \
+   LAUNCHER_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL=$(minishift console --url) \
+   --param-file=released.properties -o yaml | oc create -f -
+
+echo All set! Enjoy!
